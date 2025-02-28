fee984a9363f19f42d8c4bf803dc0cb4804d258a
Renna Sag <rennasamuel_2003@hotmail.com>
2025-02-28 18:19:23 -0300

    H2 database

--- src/main/java/com/aplicacao/entities/User.java
+++ src/main/java/com/aplicacao/entities/User.java
@@ -1,23 +1,29 @@
 package com.aplicacao.entities;
 
+import jakarta.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
-    //atributos
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String phone;
     private String password;
 
+    // Construtor vazio exigido pelo JPA
+    public User() {}
 
-    //construtores
-
+    // Construtor com argumentos
     public User(Long id, String nome, String email, String phone, String password) {
         this.id = id;
         this.nome = nome;
@@ -26,54 +32,51 @@ public class User implements Serializable {
         this.password = password;
     }
 
-
-    //getters
+    // Getters e Setters
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-
-
-    //setters
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getPhone() {
+        return phone;
+    }
+
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    //metodo hash code e equals
+    // Método hashCode e equals
     @Override
     public boolean equals(Object o) {
+        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
         return Objects.equals(id, user.id);
@@ -81,9 +84,6 @@ public class User implements Serializable {
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(id);
+        return Objects.hash(id);
     }
-
-
-
 }
